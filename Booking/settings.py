4dae2a1cb3e3ea2d09477f0edf54b65e5834ddc9
Shivam Shah <shivamshah71@gmail.com.com>
2020-07-09 02:25:17 +0530

    heroku changes

--- Booking/settings.py
+++ Booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#9ckgxs&yfq)=h358lcc*nl5a1-b4naqr4y4&j$29qz1_k#t8%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
